2e0f5347c29e894013ccff61fa6f4179ccd3eaed
조승환 <jsh1937@x-over.co.kr>
2021-04-01 17:09:14 +0900

    git test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,7 @@
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
+                            헬로우
                         </div>
                     @endif
 
